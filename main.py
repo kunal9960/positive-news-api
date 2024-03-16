12a3215ee011b34359f6263c9aa2b9229786b708
Kunal <kunald9960@gmail.com>
2024-03-16 14:12:51 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
 
-url = "https://finance.yahoo.com/"
+api_key = "1b029027b0404e2cb6c8e7cb30d8c932"
+url = ("https://newsapi.org/v2/everything?q=tesla&from="
+       "2024-02-16&sortBy=publishedAt&apiKey="
+       "1b029027b0404e2cb6c8e7cb30d8c932")
 
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
 
+# Make dictionary with data
+content = request.json()
+
+# Access the article titles and description
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
