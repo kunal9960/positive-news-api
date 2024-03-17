23ff1e67fa4fe8643e758071ebe59c0451c6b2b4
Kunal <kunald9960@gmail.com>
2024-03-17 15:08:42 +0530

    Revert "Add subject and modified url"
    This reverts commit 29a67010ff9f8423aab4b5b709ad598609dca1a5.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
 from send_email import send_email
 
-topic = "tesla"
 api_key = "1b029027b0404e2cb6c8e7cb30d8c932"
 url = ("https://newsapi.org/v2/top-headlines?"
-       "country=us&category=business&apiKey=1b029027b0404e2cb6c8e7cb30d8c932&language=en")
+       "country=us&category=business&apiKey=1b029027b0404e2cb6c8e7cb30d8c932")
 
 # Make request
 request = requests.get(url)
@@ -14,12 +13,9 @@ content = request.json()
 
 # Access the article titles and description
 body = ""
-for article in content["articles"][:20]:
+for article in content["articles"]:
     if article["title"] and article["description"] is not None:
-        body = ("Subject: Today's news" + "\n"
-                + (body + article["title"] + "\n"
-                   + article["description"] + "\n"
-                   + article["url"] + 2 * "\n"))
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
