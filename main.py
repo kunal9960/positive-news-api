98e3692a0cd4a3ee2b6762933ce499fdfeb56e92
Kunal <kunald9960@gmail.com>
2024-03-17 16:00:51 +0530

    Modify format

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 from send_email import send_email
 
 api_key = "1b029027b0404e2cb6c8e7cb30d8c932"
-url = ("https://newsapi.org/v2/top-headlines?"
-       "country=us&category=business&apiKey=1b029027b0404e2cb6c8e7cb30d8c932")
+url = ("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey="
+       "1b029027b0404e2cb6c8e7cb30d8c932")
 
 # Make request
 request = requests.get(url)
@@ -12,13 +12,10 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
-body = ""
-for article in content["articles"]:
-    if article["title"] and article["description"] is not None:
-        body = ("Subject: Today's news" + "\n"
-                + (body + article["title"] + "\n"
-                   + article["description"] + "\n"
-                   + article["url"] + 2 * "\n"))
+body = "Subject: Daily News!\n\n"
+for article in content["articles"][:10]:
+    if article["title"] and article["description"] and article["url"] is not None:
+        body += article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
